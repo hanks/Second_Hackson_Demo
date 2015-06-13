860d261cd3fca345ca3a50a692ecfe1ca93772d8
hanks <zhouhan315@gmail.com>
2015-06-13 20:45:51 +0900

    adjust heart

---
+++ BeaconCharity/ViewControllers/CharityDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  CharityDetailViewController.m
+//  BeaconCharity
+//
+//  Created by hanks on 6/13/15.
+//  Copyright (c) 2015 com.hanks. All rights reserved.
+//
+
+#import "CharityDetailViewController.h"
+
+@interface CharityDetailViewController ()
+
+@end
+
+@implementation CharityDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
